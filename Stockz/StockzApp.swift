20e721a3f58185e1de9f1ec1a2149943023bc14b
Aleksei Sobolevskii <sobolevsky.alexey@gmail.com>
2025-06-21 20:00:36 -0400

    Initial Commit

---
+++ Stockz/StockzApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockzApp.swift
+//  Stockz
+//
+//  Created by Aleksei Sobolevskii on 2025-06-21.
+//
+
+import SwiftUI
+
+@main
+struct StockzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
